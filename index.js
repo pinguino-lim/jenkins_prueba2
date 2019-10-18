be72dec1e22038c545e33a685a0d34d3e2dc3f97
HyeLim Park <pinguino.lim@gmail.com>
2019-10-18 12:40:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/notok', (req, res) => res.json({ ok: false }))
 
 app.get('/username', (req, res) => res.json({ username: process.env.USERNAME }))
 
+app.get('/username_password', (req, res) => res.json({ username_password: process.env.ID_PASSWORD,
+														username: process.env.ID_PASSWORD_USR,
+														password: process.env.ID_PASSWORD_PSW}))
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = { app };
